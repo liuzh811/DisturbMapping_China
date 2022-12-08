83ab3155fc7d69eebec95b769b47f4b48b356f13
liuzh811 <liuzh811@gmail.com>
2022-12-08 08:42:07 -0700

    Update figure5.r

--- code/figure5.r
+++ code/figure5.r
@@ -1,5 +1,6 @@
-
-## Fig 5
+## 12/8/2022
+## figure 5
+## download data from data folder first
 
 library(sf)
 library(tcltk)
@@ -45,8 +46,6 @@ r1.grd = stack(r1, r2)
 names(r1.grd) <- c("Before NFPP", "After NFPP")
 # smooth here
 for(i in 1:2){
-# r0 = focal(r1.grd[[i]], w=matrix(1/9,nrow=3,ncol=3), na.rm = TRUE)
-# r0.df = data.frame(long = chn.shp@data$x, lat = chn.shp@data$y, dat = extract(r0, data.frame(long = chn.shp@data$x, lat = chn.shp@data$y)))
 r0.df = data.frame(long = chn.shp@data$x, lat = chn.shp@data$y, dat = extract(r1.grd[[i]], data.frame(long = chn.shp@data$x, lat = chn.shp@data$y)))
 r0 = rasterFromXYZ(r0.df)
 r1.grd[[i]] <- r0
@@ -66,21 +65,12 @@ r1.grd[r1.grd < r1.range[1]] = r1.range[1]
 
 names(r1.grd) <- c("Before NFPP", "After NFPP")
 
-plot(r1.grd)
-# plot(chn.shp2, add = TRUE)
-
-png(paste0("F:/ChinaWork/DisturbMap/Results/","beforeandafter2000",".png"),height = 2500, width = 2000, res = 300, units = "px")
-
+#plot
 p.strip <- list(cex=1, lines=2, fontface='bold')
 rasterVis::levelplot(r1.grd, 
           zlim = r1.range,
-		  # main = list.title[[i]],
-		  # main = expression("Net C Uptake Trend"~ "("~gC ~ m^{-2} ~ yr ^{-1}~ yr ^{-1}~ "1980-2017" ~")"),		  
           maxpixels = nrow(r1.grd)*ncol(r1.grd),
-          # col.regions = rev(colorRampPalette(c("blue", "white", "red"))(255)),
 		  col.regions = colorRampPalette(c("#4575b4", "#ffffbf", "#d73027"))(255),
-          # scales=list(x=list(cex=1),y=list(cex=1)),
-          # xlab=list(label = "Longtitude", cex=1),ylab=list(label = "Latitude", cex=1),
 		  xlab=NULL, ylab=NULL,
 		  scales=list(draw=FALSE),
 		  layout=c(1, 2),
@@ -91,9 +81,6 @@ rasterVis::levelplot(r1.grd,
           par.strip.text=p.strip) +  
 		  latticeExtra::layer(sp.polygons(chn0.shp, col = "black", lwd = 2)) +
 		  latticeExtra::layer(sp.polygons(nfpp.shp, col = "blue", lwd = 2)) 
-
-
-dev.off()
 
 
 ######## plot histgram before and after NFPP
@@ -121,9 +108,6 @@ p <- ggplot(df, aes(x=disturb, fill=Period, color=Period)) +
 
  p + theme_bw() + 
   theme(legend.position = c(0.75, 0.75))+
-  # scale_fill_discrete(breaks=shortnames,
-                      # name="",
-                      # labels=shortnames)+
   theme(legend.text = element_text(size = 18))+
   theme(axis.title.x = element_text(face="bold", colour="black", size=18),axis.text.x  = element_text(colour="black",size=18))+
   theme(axis.title.y = element_text(face="bold", colour="black", size=18),axis.text.y  = element_text(colour="black",size=18))+
@@ -131,9 +115,6 @@ p <- ggplot(df, aes(x=disturb, fill=Period, color=Period)) +
   xlab("% Forest Disturbed") +
   theme(strip.text.x = element_text(size=18), strip.text.y = element_text(size=18)) + 
   theme(legend.title = element_text(colour="black", size=18, face="bold"))
- 
-# ggsave(paste0("F:/ChinaWork/DisturbMap/Results/","NFPP_national","_histgram.png"), width = 6, height = 4, units = "in", bg = "transparent")
- 
  
 ### only look at the NFPP regions
 nfpp.grd = rasterize(nfpp.shp,  r1.grd) 
@@ -162,9 +143,6 @@ p <- ggplot(df, aes(x=disturb, fill=Period, color=Period)) +
 
  p + theme_bw() + 
   theme(legend.position = c(0.75, 0.75))+
-  # scale_fill_discrete(breaks=shortnames,
-                      # name="",
-                      # labels=shortnames)+
   theme(legend.text = element_text(size = 18))+
   theme(axis.title.x = element_text(face="bold", colour="black", size=18),axis.text.x  = element_text(colour="black",size=18))+
   theme(axis.title.y = element_text(face="bold", colour="black", size=18),axis.text.y  = element_text(colour="black",size=18))+
@@ -173,8 +151,6 @@ p <- ggplot(df, aes(x=disturb, fill=Period, color=Period)) +
   theme(strip.text.x = element_text(size=18), strip.text.y = element_text(size=18)) + 
   theme(legend.title = element_text(colour="black", size=18, face="bold"))
   
-# ggsave(paste0("F:/ChinaWork/DisturbMap/Results/","NFPP_region","_histgram.png"), width = 6, height = 4, units = "in", bg = "transparent")
- 
 ### outside NFPP regions
 r0.grd = r1.grd
 r0.grd[nfpp.grd == 1] = NA
@@ -201,9 +177,6 @@ p <- ggplot(df, aes(x=disturb, fill=Period, color=Period)) +
 
  p + theme_bw() + 
   theme(legend.position = c(0.75, 0.75))+
-  # scale_fill_discrete(breaks=shortnames,
-                      # name="",
-                      # labels=shortnames)+
   theme(legend.text = element_text(size = 18))+
   theme(axis.title.x = element_text(face="bold", colour="black", size=18),axis.text.x  = element_text(colour="black",size=18))+
   theme(axis.title.y = element_text(face="bold", colour="black", size=18),axis.text.y  = element_text(colour="black",size=18))+
@@ -212,4 +185,3 @@ p <- ggplot(df, aes(x=disturb, fill=Period, color=Period)) +
   theme(strip.text.x = element_text(size=18), strip.text.y = element_text(size=18)) + 
   theme(legend.title = element_text(colour="black", size=18, face="bold"))
   
- # ggsave(paste0("F:/ChinaWork/DisturbMap/Results/","NFPP_outside","_histgram.png"), width = 6, height = 4, units = "in", bg = "transparent")
